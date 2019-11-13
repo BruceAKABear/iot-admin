ceda12450230b9490e62be1ed9c9d13b45e96f28
dengyi <dengyi@dengyi.pro>
2019-11-13 10:23:50 +0800

    开发登录页面1.0

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Checkbox, Form, FormItem, Input, Link } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Checkbox)
+Vue.use(Link)
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,11 +4,21 @@
     <div class="login-form">
       <!--登录表单-->
       <el-form ref="form" :model="form" label-width="80px">
-        <el-form-item label="用户名">
-          <el-input v-model="form.name"></el-input>
+        <el-form-item>
+          <el-input
+            placeholder="手机号"
+            prefix-icon="el-icon-user"
+            v-model="form.name"></el-input>
         </el-form-item>
-        <el-form-item label="密码">
-          <el-input v-model="form.password"></el-input>
+        <el-form-item>
+          <el-input
+            placeholder="密码"
+            prefix-icon="el-icon-lock"
+            v-model="form.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox v-model="checked">记住账号</el-checkbox>
+          <el-link type="info">忘记密码</el-link>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="onSubmit">立即创建</el-button>
@@ -23,14 +33,14 @@
 
 <script>
 export default {
-  name: 'Longin',
+  name: 'Login',
   data () {
     return {
       form: {
         name: '',
         password: ''
-
       },
+      checked: true,
       rules: {
         username: [
           { required: true, message: '请输入账号', trigger: 'submit' }
@@ -69,5 +79,9 @@ export default {
   .login-main {
     width: 100%;
     height: 100%;
+  }
+
+  .login-form {
+    width: 400px;
   }
 </style>
