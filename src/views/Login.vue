ff76b890b6dd0726d5525556abadb652016013e6
dengyi <916295934@qq.com>
2019-12-16 21:29:35 +0800

    页面布局

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,40 +2,49 @@
   <!--登录页面-->
   <div class="login-page-container">
     <div class="login-box">
-      <div class="login-box-header">
-        用户名密码登录
+      <div class="login-box-left">
+        <img src="../assets/personal-log.jpg" height="200" width="250"/>
       </div>
-      <div class="login-box-body">
-        <el-form ref="form" :model="form" :rules="rules">
-          <el-form-item prop="username">
-            <el-input
-              placeholder="手机号"
-              prefix-icon="el-icon-user"
-              v-model="form.username">
-            </el-input>
-          </el-form-item>
-          <el-form-item prop="password">
-            <el-input
-              placeholder="密码"
-              prefix-icon="el-icon-lock"
-              type="password"
-              v-model="form.password">
-            </el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-row>
-              <el-col :span="12" align="left">
-                <el-checkbox v-model="checked">记住账号</el-checkbox>
-              </el-col>
-              <el-col :span="12" align="right">
-                <el-link type="info">忘记密码</el-link>
-              </el-col>
-            </el-row>
-          </el-form-item>
-          <el-form-item>
-            <el-button type="primary" class="form-btn" @click="onSubmit('form')">登录</el-button>
-          </el-form-item>
-        </el-form>
+      <div class="login-box-right">
+        <div class="login-box-header">
+          <div><span>密码登录</span></div>
+          <div>
+            <span>扫码登录</span>
+            <img src="../assets/二维码.png" height="44" width="44"/>
+          </div>
+        </div>
+        <div class="login-box-body">
+          <el-form ref="form" :model="form" :rules="rules">
+            <el-form-item prop="username">
+              <el-input
+                placeholder="手机号"
+                prefix-icon="el-icon-user"
+                v-model="form.username">
+              </el-input>
+            </el-form-item>
+            <el-form-item prop="password">
+              <el-input
+                placeholder="密码"
+                prefix-icon="el-icon-lock"
+                type="password"
+                v-model="form.password">
+              </el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-row>
+                <el-col :span="12" align="left">
+                  <el-checkbox v-model="checked">记住账号</el-checkbox>
+                </el-col>
+                <el-col :span="12" align="right">
+                  <el-link type="info">忘记密码</el-link>
+                </el-col>
+              </el-row>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" class="form-btn" @click="onSubmit('form')">登录</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
       </div>
     </div>
     <div class="login-footer"></div>
@@ -124,23 +133,31 @@ export default {
 </script>
 
 <style scoped>
-  .login-page-container{
+  .login-page-container {
     height: 100%;
     background-color: #2c3e50;
   }
+
   .login-box {
-    width: 400px;
-    border-radius: 5px;
+    border-radius: 10px;
     padding: 20px;
     position: absolute;
-    left: 70%;
-    top: 40%;
+    left: 50%;
+    top: 50%;
     background-color: white;
+    transform: translate(-50%, -50%);
+    display: flex;
   }
-  .login-box-header{
-    color: red;
+  .login-box-left{
+    width: 300px;
   }
-  .login-box-body{
+
+  .login-box-header {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .login-box-body {
     margin-top: 10px;
   }
 
